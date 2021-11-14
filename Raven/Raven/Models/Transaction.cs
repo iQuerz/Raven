c0f836753015f53b436f2660d472311d9f529150
iQuerz <niki.tenis@gmail.com>
2021-11-14 20:18:45 +0100

    First commit

---
+++ Raven/Raven/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models
+{
+    public class Transaction
+    {
+        public int _ID { get; set; }
+        //TODO: add other attributes and make children classes.
+    }
+}
