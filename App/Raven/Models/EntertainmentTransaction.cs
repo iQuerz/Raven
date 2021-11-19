69254ab0be2bf91f1c43835212fd0692455a7e62
JoeMlah <mladenvmladenovic@gmail.com>
2021-11-19 16:57:45 +0100

    add EntertainmentTransaction

---
+++ App/Raven/Models/EntertainmentTransaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Models.Types;
+namespace App.Models
+{
+   public  class EntertainmentTransaction : Transaction
+    {
+        public EntertainmentType _EntertainmentType { get; set; }
+    }
+}
