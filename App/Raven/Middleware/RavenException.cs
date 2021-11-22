d9a24dbaf2a10009d0b70ff27e88cdd9984eaf4b
iQuerz <niki.tenis@gmail.com>
2021-11-22 01:44:17 +0100

    Added Logging Capability and custom RavenException

---
+++ App/Raven/Middleware/RavenException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace App.Middleware
+{
+    public class RavenException : Exception
+    {
+        public RavenException()
+            : base()
+        {
+
+        }
+
+        public RavenException(string message)
+            : base(message)
+        {
+
+        }
+
+        public static void Log(Exception e)
+        {
+            // TODO: A good dynamic way of setting path of log files. Possibly saving it in AppSettings table.
+            // TODO: Delete logs from previous version(s) on update.
+            // TODO: Automated logs upload somewhere?
+
+            // create folder if needed
+            if (!Directory.Exists("logs"))
+                Directory.CreateDirectory("logs");
+
+            // File names have "YYYY-MM-DD_HH-mm" format.
+            DateTime date = DateTime.Now;
+            string filename = $"{date.Year}-{date.Month}-{date.Day}_{date.Hour}-{date.Minute}.log";
+
+            // log the exception
+            StreamWriter sw = new StreamWriter($"logs\\{filename}");
+
+            sw.WriteLine($"{DateTime.Now} in {e.Source}.\n");
+            sw.WriteLine(e.Message + "\n");
+            sw.WriteLine(e.StackTrace + "\n");
+
+            sw.Dispose();
+        }
+    }
+}
