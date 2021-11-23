c0600f0c13fe5b0b83002aeda768ec765caac3e2
Jovan Rasic <joca.lego@gmail.com>
2021-11-23 19:59:38 +0100

    Added Income, Grocery, Bills, BeautyAndFashion classes

---
+++ App/Raven/Models/GroceryTransaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models
+{
+    internal class GroceryTransaction : Transaction
+    {
+        //to be added
+    }
+}
---
+++ App/Raven/Models/IncomeTransaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Models.Types;
+
+namespace App.Models
+{
+    internal class IncomeTransaction : Transaction
+    {
+        public IncomeTransactionType _incomeTransactionType { get; set; }
+    }
+}
