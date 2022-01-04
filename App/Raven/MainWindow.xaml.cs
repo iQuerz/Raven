c10ccf9e23e626d5e5c4231cc197e828887c8553
iQuerz <niki.tenis@gmail.com>
2022-01-04 04:41:10 +0100

    Added AutoSave functionality.

--- App/Raven/MainWindow.xaml.cs
+++ App/Raven/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Raven
 {
@@ -22,10 +23,19 @@ namespace Raven
     /// </summary>
     public partial class MainWindow : Window
     {
+        Manager _manager;
         public MainWindow()
         {
             InitializeComponent();
+            _manager = new Manager();
             Loaded += App_OnLoad;
+
+            #region AutoSave Timer Setup
+            DispatcherTimer autoSaveTimer = new DispatcherTimer();
+            autoSaveTimer.Interval = TimeSpan.FromMinutes(AppSettings.AutoSavePeriod);
+            autoSaveTimer.Tick += AutoSaveTimer_Tick;
+            autoSaveTimer.Start();
+            #endregion
         }
 
         private void App_OnLoad(object sender, RoutedEventArgs e)
@@ -39,6 +49,10 @@ namespace Raven
 
             List<Transaction> testList = DBInterface.LoadData();
             DBInterface.SaveData(ref testList);
+        }
+        private void AutoSaveTimer_Tick(object sender, EventArgs e)
+        {
+            _manager.saveTransactions();
         }
     }
 }
