f172c15e92bb21474e5394b7be89a968aa705298
iQuerz <niki.tenis@gmail.com>
2022-01-04 01:05:04 +0100

    Fixed todo script formatting.

--- findTODO.py
+++ findTODO.py
@@ -1,14 +1,14 @@
 ### Python Script for Extracting tasks form .cs files
 ##  Creator itsaghin
 
-import os 
+import os
 
-def printFile(out,file, element):
+def printFile(out, file, element):
     print("File name: " + element)
     print("---------------------------------")
     while out != "":
         if(out.find("TO DO") > -1 or out.find("TODO") > -1):
-            print(out[0:len(out)-1])
+            print("     " + out[0:len(out)-1].strip())
         out = file.readline()
     print("---------------------------------\n")
 
@@ -19,17 +19,17 @@ def search(path):
     for element in inDir:
         if element.endswith(".cs") or element.endswith(".xaml") or element.endswith(".txt"):
             file = open(os.path.join(path, element), "r")
-            isEmpty = True 
+            isEmpty = True
             out = file.readline()
             while out != "":
                 if(out.find("TO DO") > -1 or out.find("TODO") > -1):
                     isEmpty = False
-                    break;    
+                    break;
                 out = file.readline()
             if(not isEmpty):
                 printFile(out,file,element)
             if os.path.isdir(os.path.join(path, element)):
-                return 
+                return
         elif os.path.isdir(os.path.join(path, element)):
             fullpath = search(os.path.join(path, element))
             if fullpath is not None:
